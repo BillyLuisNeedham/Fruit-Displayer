f7c2fc4606c3241bce4177725bb7ae26fb29e5c7
BillyLuisNeedham <billyluisneedham@gmail.com>
2021-07-01 17:38:12 +0100

    converted MainActivity.kt into DaggerAppCompatActivity

--- app/src/main/java/com/billyluisneedham/bbctest/view/MainActivity.kt
+++ app/src/main/java/com/billyluisneedham/bbctest/view/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.billyluisneedham.bbctest.view
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.billyluisneedham.bbctest.R
+import dagger.android.support.DaggerAppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : DaggerAppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
