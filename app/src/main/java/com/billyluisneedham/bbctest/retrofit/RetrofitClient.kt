c4925c47dc157bf62c13c850d9bd7c132d1333c9
BillyLuisNeedham <billyluisneedham@gmail.com>
2021-07-01 19:32:29 +0100

    RetrofitClient singleton instantiation build out complete

--- app/src/main/java/com/billyluisneedham/bbctest/retrofit/RetrofitClient.kt
+++ app/src/main/java/com/billyluisneedham/bbctest/retrofit/RetrofitClient.kt
@@ -7,36 +7,33 @@ import okhttp3.logging.HttpLoggingInterceptor.Level
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-interface RetrofitClient {
+object RetrofitClient {
 
-    companion object {
+    private const val BASE_URL =
+        "https://raw.githubusercontent.com/fmtvp/recruit-test-data/master/"
 
-        const val BASE_URL =
-            "https://raw.githubusercontent.com/fmtvp/recruit-test-data/master/"
+    private val retrofitClient: Retrofit.Builder by lazy {
 
-        val retrofitClient: Retrofit.Builder by lazy {
+        val levelType: Level = if (BuildConfig.BUILD_TYPE.contentEquals("debug"))
+            Level.BODY else Level.NONE
 
-            val levelType: Level
-            if (BuildConfig.BUILD_TYPE.contentEquals("debug"))
-                levelType = Level.BODY else levelType = Level.NONE
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(levelType)
 
-            val logging = HttpLoggingInterceptor()
-            logging.setLevel(levelType)
+        val okHttpClient = OkHttpClient.Builder()
+        okHttpClient.addInterceptor(logging)
 
-            val okHttpClient = OkHttpClient.Builder()
-            okHttpClient.addInterceptor(logging)
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient.build())
+            .addConverterFactory(GsonConverterFactory.create())
 
-            Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .client(okHttpClient.build())
-                .addConverterFactory(GsonConverterFactory.create())
+    }
 
-        }
+    val service: Service by lazy {
+        retrofitClient
+            .build()
+            .create(Service::class.java)
 
-        val service: Service by lazy {
-            retrofitClient
-                .build()
-                .create(Service::class.java)
-        }
     }
 }
