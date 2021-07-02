9a9d95ab2df1b90b47e699f9d6282bb0f7a3a3cc
BillyLuisNeedham <billyluisneedham@gmail.com>
2021-07-02 15:03:26 +0100

    MockFruit now an object

--- app/src/sharedTest/java/com/billyluisneedham/bbctest/mocks/MockFruit.kt
+++ app/src/sharedTest/java/com/billyluisneedham/bbctest/mocks/MockFruit.kt
@@ -4,29 +4,27 @@ import com.billyluisneedham.bbctest.models.Fruit
 import com.billyluisneedham.bbctest.models.FruitListResponse
 import com.billyluisneedham.bbctest.models.FruitResponse
 
-class MockFruit {
+object MockFruit {
 
-    companion object {
-        const val MOCK_TYPE = "apple"
-        const val MOCK_WEIGHT = 800
-        const val MOCK_PRICE = 120
+    const val MOCK_TYPE = "apple"
+    const val MOCK_WEIGHT = 800
+    const val MOCK_PRICE = 120
 
-        val mockFruitResponse = FruitResponse(
-            type = MOCK_TYPE,
-            weight = MOCK_WEIGHT,
-            price = MOCK_PRICE
-        )
+    val mockFruitResponse = FruitResponse(
+        type = MOCK_TYPE,
+        weight = MOCK_WEIGHT,
+        price = MOCK_PRICE
+    )
 
-        val mockFruit = Fruit(
-            type = MOCK_TYPE,
-            weight = MOCK_WEIGHT,
-            price = MOCK_PRICE
-        )
+    val mockFruit = Fruit(
+        type = MOCK_TYPE,
+        weight = MOCK_WEIGHT,
+        price = MOCK_PRICE
+    )
 
-        val listOfMockFruitResponse = listOf(mockFruitResponse)
+    private val listOfMockFruitResponse = listOf(mockFruitResponse)
 
-        val mockFruitListResponse = FruitListResponse(fruits = listOfMockFruitResponse)
+    val mockFruitListResponse = FruitListResponse(fruits = listOfMockFruitResponse)
 
 
-    }
 }
