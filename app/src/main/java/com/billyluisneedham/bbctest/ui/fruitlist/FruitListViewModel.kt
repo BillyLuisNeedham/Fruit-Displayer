b505dc187fdad5845d32f11633d8060a7a48f140
BillyLuisNeedham <billyluisneedham@gmail.com>
2021-07-03 11:09:14 +0100

    FruitListViewModel and test refactored to use live data and resource from repository

--- app/src/main/java/com/billyluisneedham/bbctest/ui/fruitlist/FruitListViewModel.kt
+++ app/src/main/java/com/billyluisneedham/bbctest/ui/fruitlist/FruitListViewModel.kt
@@ -3,13 +3,13 @@ package com.billyluisneedham.bbctest.ui.fruitlist
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.asLiveData
 import com.billyluisneedham.bbctest.models.Fruit
 import com.billyluisneedham.bbctest.source.FruitRepository
+import com.billyluisneedham.bbctest.utils.Resource
 
 class FruitListViewModel(fruitRepository: FruitRepository) : ViewModel() {
 
-    val fruitList: LiveData<List<Fruit>> = fruitRepository.getFruits().asLiveData()
+    val fruitList: LiveData<Resource<List<Fruit>>> = fruitRepository.getFruits()
 
     class Factory(private val fruitRepository: FruitRepository) : ViewModelProvider.Factory {
         @Suppress("UNCHECKED_CAST")
--- app/src/test/java/com/billyluisneedham/bbctest/ui/fruitlist/FruitListViewModelTest.kt
+++ app/src/test/java/com/billyluisneedham/bbctest/ui/fruitlist/FruitListViewModelTest.kt
@@ -1,11 +1,12 @@
 package com.billyluisneedham.bbctest.ui.fruitlist
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
 import com.billyluisneedham.bbctest.mocks.MockFruit
 import com.billyluisneedham.bbctest.source.FruitRepository
+import com.billyluisneedham.bbctest.utils.Resource
 import io.mockk.coEvery
 import io.mockk.mockk
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.runBlocking
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
@@ -21,22 +22,22 @@ class FruitListViewModelTest {
 
 
     @Test
-    fun fruitListLoadsOnInit_flowOfFruitListReturnedByFruitRepository_fruitListFromRepositoryLoadedOnInitOfViewModel() {
+    fun fruitListLoadsOnInit_liveDataOfFruitListWrappedInResultReturnedByFruitRepository_fruitListFromRepositoryLoadedOnInitOfViewModel() {
         runBlocking {
-            val fruit = MockFruit.mockFruit
-            val flow = flow {
-                emit(listOf(fruit))
-            }
+            val fruitListResource = Resource.success(listOf(MockFruit.mockFruit))
+            val liveDataFruit = MutableLiveData(fruitListResource)
 
             coEvery {
                 mockFruitRepository.getFruits()
-            } returns flow
+            } returns liveDataFruit
 
             fruitListViewModel = FruitListViewModel(mockFruitRepository)
 
+
             fruitListViewModel.fruitList.observeForever {}
 
-            assertThat(fruitListViewModel.fruitList.value, `is`(listOf(fruit)))
+
+            assertThat(fruitListViewModel.fruitList.value, `is`(fruitListResource))
         }
     }
 
