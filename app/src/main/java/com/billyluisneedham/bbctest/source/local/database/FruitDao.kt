62726f4fea4b6fd5d9655dcc50f0fb63eed59686
BillyLuisNeedham <billyluisneedham@gmail.com>
2021-07-03 09:09:23 +0100

    FruitDao now returns live data

--- app/src/main/java/com/billyluisneedham/bbctest/source/local/database/FruitDao.kt
+++ app/src/main/java/com/billyluisneedham/bbctest/source/local/database/FruitDao.kt
@@ -1,21 +1,21 @@
 package com.billyluisneedham.bbctest.source.local.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy.REPLACE
 import androidx.room.Query
 import com.billyluisneedham.bbctest.models.Fruit
-import com.billyluisneedham.bbctest.source.local.LocalFruitDataSource
-import kotlinx.coroutines.flow.Flow
+import com.billyluisneedham.bbctest.source.local.ILocalFruitDataSource
 
 @Dao
-interface FruitDao: LocalFruitDataSource {
+interface FruitDao: ILocalFruitDataSource {
 
     @Insert(onConflict = REPLACE)
     override suspend fun saveFruits(fruits: List<Fruit>)
 
     @Query("SELECT * FROM Fruit")
-    override fun getAllFruits(): Flow<List<Fruit>>
+    override fun getAllFruits(): LiveData<List<Fruit>>
 
     @Query("DELETE From Fruit")
     suspend fun deleteAll()
