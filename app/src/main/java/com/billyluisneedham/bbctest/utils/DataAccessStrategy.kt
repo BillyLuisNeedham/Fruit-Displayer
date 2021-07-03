8f0b48a2725db8e1913457d221ceb72e48871858
BillyLuisNeedham <billyluisneedham@gmail.com>
2021-07-03 12:18:28 +0100

    DataAccessStrategy updated for flow returned by database

--- app/src/main/java/com/billyluisneedham/bbctest/utils/DataAccessStrategy.kt
+++ app/src/main/java/com/billyluisneedham/bbctest/utils/DataAccessStrategy.kt
@@ -1,25 +1,27 @@
 package com.billyluisneedham.bbctest.utils
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.liveData
-import androidx.lifecycle.map
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 fun <T, A> performGetOperation(
-    databaseQuery: () -> LiveData<T>,
+    databaseQuery: () -> Flow<T>,
     networkCall: suspend () -> Resource<A>,
     saveCallResult: suspend (A) -> Unit
 ): LiveData<Resource<T>> =
     liveData(Dispatchers.IO) {
         emit(Resource.loading())
         val source = databaseQuery.invoke().map { Resource.success(it) }
-        emitSource(source)
+        emitSource(source.asLiveData())
 
         val responseStatus = networkCall.invoke()
         if (responseStatus.status == Resource.Status.SUCCESS) {
             saveCallResult(responseStatus.data!!)
         } else if (responseStatus.status == Resource.Status.ERROR) {
             emit(Resource.error(responseStatus.message!!))
-            emitSource(source)
+            emitSource(source.asLiveData())
         }
     }
