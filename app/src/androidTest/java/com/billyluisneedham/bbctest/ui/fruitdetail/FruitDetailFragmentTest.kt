6ef1b1e848d37fe1f6a8c20ac7cff960ef16e3a5
BillyLuisNeedham <billyluisneedham@gmail.com>
2021-07-04 20:44:08 +0100

    FruitDetailFragment build complete with tests

--- app/src/androidTest/java/com/billyluisneedham/bbctest/ui/fruitdetail/FruitDetailFragmentTest.kt
+++ app/src/androidTest/java/com/billyluisneedham/bbctest/ui/fruitdetail/FruitDetailFragmentTest.kt
@@ -6,26 +6,66 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.platform.app.InstrumentationRegistry
+import com.billyluisneedham.bbctest.R
 import com.billyluisneedham.bbctest.mocks.MockFruit
 import com.billyluisneedham.bbctest.ui.fruitdetail.FruitDetailFragment.Companion.ARG_PRICE
 import com.billyluisneedham.bbctest.ui.fruitdetail.FruitDetailFragment.Companion.ARG_TYPE
 import com.billyluisneedham.bbctest.ui.fruitdetail.FruitDetailFragment.Companion.ARG_WEIGHT
 import com.billyluisneedham.bbctest.utils.capitalise
+import org.junit.Before
 import org.junit.Test
+import java.text.NumberFormat
+import java.util.*
 
 class FruitDetailFragmentTest {
+    private val fragmentArgs = bundleOf(
+        ARG_TYPE to MockFruit.mockFruit.type,
+        ARG_PRICE to MockFruit.mockFruit.price,
+        ARG_WEIGHT to MockFruit.mockFruit.weight
+    )
+    private lateinit var weightMessage: String
+    private lateinit var priceMessage: String
+    private lateinit var kg: String
 
+    @Before
+    fun setUp() {
+        weightMessage =
+            InstrumentationRegistry.getInstrumentation().targetContext.getString(R.string.weight)
+        priceMessage = InstrumentationRegistry.getInstrumentation().targetContext.getString(R.string.price)
+        kg = InstrumentationRegistry.getInstrumentation().targetContext.getString(R.string.kg)
+    }
 
     @Test
-    fun displaysCapitalisedTypeOfFruitPassedByArgs() {
-        val fragmentArgs = bundleOf(
-            ARG_TYPE to MockFruit.mockFruit.type,
-            ARG_PRICE to MockFruit.mockFruit.price,
-            ARG_WEIGHT to MockFruit.mockFruit.weight
-        )
-        launchFragmentInContainer<FruitDetailFragment>(fragmentArgs)
+    fun fruitType_typePassedViaArgs_displaysCapitalisedTypeOfFruitPassedByArgs() {
+        initFragmentForTests()
 
         onView(withText(MockFruit.mockFruit.type.capitalise()))
+            .check(matches(isDisplayed()))
+    }
+
+    private fun initFragmentForTests() {
+        launchFragmentInContainer<FruitDetailFragment>(fragmentArgs)
+
+    }
+
+    @Test
+    fun fruitWeight_weightPassedViaArgs_displaysTheWeightConvertedFromGramsToKgsWithCorrectMessage() {
+        initFragmentForTests()
+        val expectedWeight: Double = MockFruit.mockFruit.weight.toDouble() / 1000
+
+        onView(withText("$weightMessage $expectedWeight$kg"))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun fruitPrice_pricePassedViaArgs_displaysThePriceConvertedFromPenceToPoundWithCorrectMessageAndZeroAppendedToPriceIfRequired() {
+        initFragmentForTests()
+        val price: Double = MockFruit.mockFruit.price.toDouble()
+        val n = NumberFormat.getCurrencyInstance(Locale.UK)
+        val expectedPrice = n.format(price / 100)
+
+        onView(withText("$priceMessage $expectedPrice"))
             .check(matches(isDisplayed()))
     }
 }
--- app/src/main/java/com/billyluisneedham/bbctest/ui/fruitdetail/FruitDetailFragment.kt
+++ app/src/main/java/com/billyluisneedham/bbctest/ui/fruitdetail/FruitDetailFragment.kt
@@ -5,13 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.billyluisneedham.bbctest.R
 import com.billyluisneedham.bbctest.databinding.FragmentDetailBinding
 import com.billyluisneedham.bbctest.models.Fruit
 import com.billyluisneedham.bbctest.utils.capitalise
+import java.text.NumberFormat
+import java.util.*
 
 class FruitDetailFragment : Fragment() {
 
     companion object {
+        private const val WEIGHT_STRING = R.string.weight
+        private const val KG = R.string.kg
+        private const val PRICE_STRING = R.string.price
         const val ARG_TYPE = "arg_type"
         const val ARG_PRICE = "arg_price"
         const val ARG_WEIGHT = "arg_weight"
@@ -44,11 +50,38 @@ class FruitDetailFragment : Fragment() {
 
     private fun setUiDetailsFromArguments() {
         binding.tvFruitType.text = getTypeFromArgs()
+        binding.tvWeight.text = getWeightMessage()
+        binding.tvPrice.text = getPriceMessage()
+    }
 
+    private fun getPriceMessage(): String {
+        val priceMessage = requireContext().getString(PRICE_STRING)
+        val priceInPence = requireArguments().getInt(ARG_PRICE)
+        val priceInPounds = convertPenceToPounds(priceInPence.toDouble())
+        return "$priceMessage $priceInPounds"
+    }
+
+    private fun convertPenceToPounds(priceInPence: Double): String {
+        val n = NumberFormat.getCurrencyInstance(Locale.UK)
+        return n.format(priceInPence / 100)
     }
 
     private fun getTypeFromArgs(): String {
         val type = requireArguments().getString(ARG_TYPE)
         return type?.capitalise() ?: throw IllegalStateException("$ARG_TYPE should not be null")
+    }
+
+    private fun getWeightMessage(): String {
+        val weightInKg = getWeightInKgFromArgs()
+        val prependWeightMessage = requireContext().getString(WEIGHT_STRING)
+        val kg = requireContext().getString(KG)
+
+        return "$prependWeightMessage $weightInKg$kg"
+    }
+
+    private fun getWeightInKgFromArgs(): String {
+        val weight = requireArguments().getInt(ARG_WEIGHT)
+        val convertedWeight = weight.toDouble() / 1000
+        return convertedWeight.toString()
     }
 }
