76185b76d8cdd366a6707e33b12cb9ab372f3801
BillyLuisNeedham <billyluisneedham@gmail.com>
2021-07-09 10:41:03 +0100

    FruitDao no longer inherits from ILocalFruitDataSource, ILocalFruitDataSource deleted

--- app/src/main/java/com/billyluisneedham/fruitlist/source/local/database/FruitDao.kt
+++ app/src/main/java/com/billyluisneedham/fruitlist/source/local/database/FruitDao.kt
@@ -5,18 +5,17 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy.REPLACE
 import androidx.room.Query
 import com.billyluisneedham.fruitlist.models.Fruit
-import com.billyluisneedham.fruitlist.source.local.ILocalFruitDataSource
 import kotlinx.coroutines.flow.Flow
 
 @Dao
-interface FruitDao: ILocalFruitDataSource {
+interface FruitDao {
 
     @Insert(onConflict = REPLACE)
-    override suspend fun saveFruits(fruits: List<Fruit>)
+    suspend fun saveFruits(fruits: List<Fruit>)
 
     @Query("SELECT * FROM Fruit")
-    override fun getAllFruits(): Flow<List<Fruit>>
+    fun getAllFruits(): Flow<List<Fruit>>
 
     @Query("DELETE From Fruit")
-    override suspend fun deleteAllFruits()
+    suspend fun deleteAllFruits()
 }
